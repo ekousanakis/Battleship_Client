462e406c7431f46b158a71775ae2b979db7b7db4
alexalepis <aalepis@NT-21.tcb.local>
2018-03-08 18:10:59 +0200

    feat(battleship client) connect node to server

--- lib/battleship_client.ex
+++ lib/battleship_client.ex
@@ -1,13 +1,26 @@
 defmodule BattleshipClient do
 
-  def start(server_name) do
+  def join(server_name, username) do
 
-    Node.set_cookie(Node.self, :"test")
+     Node.set_cookie(Node.self, :"test")
 
     case Node.connect(server_name) do
-      true   -> :ok
+      true   -> Process.send({:game_server ,server_name}, {:join, username}, [])
       reason -> IO.puts "Could not connect to server, reason: #{reason}"
-                System.halt(0)
     end
+
+    # Ston Server:    iex --sname server -S mix
+
+    # Ston client :   iex --sname cl1 -S mix
+    #                 BattleshipClient.join(:"server@NT-21", "alex")
+
+
+
+
+
+
+
+
+
   end
 end
